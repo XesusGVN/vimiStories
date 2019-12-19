b5c46b3642f82d2f3f8990c8f660cf829dc95d93
Edward <edwardshumkov@gmail.com>
2019-12-19 21:32:34 +0500

    add registration

--- class/bot/Events/onMessage.js
+++ class/bot/Events/onMessage.js
@@ -1,10 +1,84 @@
+const USER      =  require('../../user/user');
+const waitSync  = require('wait-sync');
 
 module.exports.onMessage = function(){
     const logger = require('../../logs/logfunc');
     global.bot.on('message', (msg) => {
-        var bot = global.bot;
-        bot.sendMessage(msg.from.id, msg.text);
-
+        var bot  = global.bot;
+        var id   = msg.from.id;
+        var text = msg.text;
+        // bot.sendMessage(msg.from.id, msg.text);
+        var h_user = USER.haveUser(id);
         logger.log("@" + msg.from.username + ": " + msg.text);
+
+
+        if (h_user == 1){
+            // Если пользователь Зарегистрирован
+            var USER_ACTION = USER.userAction(id);
+            if(USER_ACTION == 'GET_YOUR_NAME'){
+                if(!(text.length <= 3 || text.length >= 18 || typeof text == undefined || typeof text == "undefined")){
+                    var setName = USER.setName(id,text);
+
+                    if(setName == true){
+                        USER.setAction(id,'GET_YOUR_CONTACT_DATA');
+                        var options = {
+                            "parse_mode": "html",
+                            "reply_markup": JSON.stringify({
+                                "resize_keyboard": true,
+                                "keyboard": [
+                                    [{ text: "🔰Отправить Контакт", request_contact: true }]
+                                ],
+                                "one_time_keyboard" : true
+                            })
+                        };
+                        bot.sendMessage(id, "<b>Шаг 2: </b><i>Отправьте нам свои контактные данные</i>", options);
+                    }
+                }else{
+                    bot.sendMessage(id,"<b>❌Ошибка: Укажите Действующее Имя пользователя</b>", {parse_mode: 'html'});
+                }
+            }else if(USER_ACTION == 'GET_YOUR_CONTACT_DATA'){
+                if(msg.contact){
+                    if(msg.contact.phone_number.length > 7){
+                        var setContact = USER.setContact(msg.contact);
+
+                        if(setContact){
+                            // Вызов Главного Меню
+                            USER.setAction(id,'DEFAULT');
+                            bot.sendMessage(id, "<b>✅Вы успешно Зарегистрировались</b>", {parse_mode: 'html'});
+                        }
+                    }else{
+                        bot.sendMessage(id,"<b>❌Ошибка: Укажите в настройках аккаунта Telegram номер мобильного телефона</b>", {parse_mode: 'html'});
+                    }
+                }else{
+                    USER.setAction(id,'GET_YOUR_CONTACT_DATA');
+                        var options = {
+                            "parse_mode": "html",
+                            "reply_markup": JSON.stringify({
+                                "resize_keyboard": true,
+                                "keyboard": [
+                                    [{ text: "🔰Отправить Контакт", request_contact: true }]
+                                ],
+                                "one_time_keyboard" : true
+                            })
+                        };
+                        bot.sendMessage(id, "<b>Ничего писать не нужно</b>, <i>просто нажмите кнопку </i> <b>🔰Отправить Контакт</b>", options);
+                }
+            }
+        }else{
+            if(text == '/start'){
+                // Зарегистрировать
+                var reg = USER.regUser(msg.from);
+            
+                if(reg == 1){
+                    bot.sendMessage(id,`<b>Шаг 1:</b> <i>Укажите своё настоящее имя</i>
+<b>Например:</b> <i>Эдуард</i>
+                    `, {parse_mode: 'html'});
+
+                    USER.setAction(id,'GET_YOUR_NAME');
+                }
+            }else{
+                bot.sendMessage(id,"<b>‼️Вы не зарегистрированны в системе, для регистрации пропишите /start</b>", {parse_mode: 'html'});
+            }
+        }
     });
 }
---
+++ class/user/user.js
@@ -0,0 +1,108 @@
+const waitSync  = require('wait-sync');
+
+var haveUser = function(id){
+       var status = 0;
+       var req =  global.connection.query('SELECT 1 FROM users WHERE TG_ID = ?', id, function(err,res){
+            if(err) throw err;
+            else{
+                if(res.length > 0){
+                   status = 1;
+                }
+            }
+        });
+        waitSync(0.1);
+        if(req){
+           return status;
+        }
+}
+
+var regUser = function(data){
+    var USER = {
+        TG_ID: data.id,
+        USER_NAME: data.username,
+        USER_ACTION: 'GET_NAME'
+    }
+    var status = 0;
+
+    if(USER){
+        var req = global.connection.query('INSERT INTO users SET ?', USER, function(err){
+            if(err) throw err;
+            else{
+                status = 1;
+            }
+        });
+        waitSync(0.1);
+        if(req){
+            return status;
+        }
+    }
+}
+
+var userAction = function(id){
+    var action = null;
+    var req = global.connection.query('SELECT * FROM users WHERE TG_ID = ?', id, function(err,res){
+        if(err) throw err;
+        else{
+            if(res.length > 0){
+                action = res[0].USER_ACTION;
+            }
+        }
+    });
+    waitSync(0.1);
+    if(req){
+        return action;
+    }
+}
+
+var setAction = function(id,action){
+    var callback;
+    var req = global.connection.query('UPDATE users SET ? WHERE TG_ID = ?', [{USER_ACTION: action}, id], function(err,field){
+        if(err) throw err;
+        else{
+            callback = 1;
+        }
+    });
+    waitSync(0.1);
+
+    if(req){
+        return callback;
+    }
+}
+
+var setName = function(id,name){
+    var callbaack;
+    var req = global.connection.query('UPDATE users SET ? WHERE TG_ID = ?', [{NAME: name}, id], function(err){
+        if(err) throw err;
+        else{
+            callbaack = true;
+        }
+    });
+    waitSync(0.1);
+    if(req){
+        return callbaack;
+    }
+}
+
+var setContact = function(data){
+    var callback;
+    var req = global.connection.query('UPDATE users SET ? WHERE TG_ID = ?', [{TEL_NUMBER: data.phone_number},data.user_id], function(err){
+        if(err) throw err;
+        else{
+            callback = true;
+        }
+    });
+
+    waitSync(0.1);
+    if(req){
+        return callback;
+    }
+}
+
+
+
+module.exports.haveUser = haveUser;
+module.exports.regUser  = regUser;
+module.exports.userAction = userAction;
+module.exports.setAction = setAction;
+module.exports.setName = setName;
+module.exports.setContact = setContact;
